ee7d9075b5c129e4330977d54e57975a19fed4a2
Aaron Andersen <aanderse@tvbeat.com>
2025-10-20 11:31:25 -0400

    move source to src/ subdirectory

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         packages.default = pkgs.buildGoModule {
           pname = "tvbeat-ssh";
           version = "1.0.1";
-          src = ./.;
+          src = ./src;
           vendorHash = "sha256-zrjQaDF/h5Lq2vivJ9vDAw1kTOh7cmHH3z1iBhewpzY=";
         };
 

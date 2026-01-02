7390c2a65fbc1a2fd4cfc62e212bc29cf66748d9
janhencic <jan@hencic.com>
2026-01-02 14:30:08 +0100

    Update go module vendor hash
    It seems the hash does not match. I generated the new hash with:
    
    ```
    go mod vendor
    nix hash path vendor
    ```

--- overlays.nix
+++ overlays.nix
@@ -3,6 +3,6 @@ final: _: {
     pname = "tvbeat-ssh";
     version = "1.0.1";
     src = ./src;
-    vendorHash = "sha256-zrjQaDF/h5Lq2vivJ9vDAw1kTOh7cmHH3z1iBhewpzY=";
+    vendorHash = "sha256-ZHl0rME6pgHbIb2TMjDrcqMIJJ+HNGyc2SlVb6amtS8=";
   };
 }

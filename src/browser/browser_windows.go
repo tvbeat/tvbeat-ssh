2d32ea98881eac7d93be708a1519182353a91153
Aaron Andersen <aanderse@tvbeat.com>
2026-01-13 12:47:34 -0500

    fixup: windows build

--- src/browser/browser_windows.go
+++ src/browser/browser_windows.go
@@ -1,10 +1,14 @@
 package browser
 
-import "golang.org/x/sys/windows"
+import (
+	"errors"
+
+	"golang.org/x/sys/windows"
+)
 
 func openBrowser(url, app string) error {
 	if app != "" {
-		fmt.Errorf("not implemented: cannot override browser on Windows")
+		return errors.New("not implemented: cannot override browser on Windows")
 	}
 
 	return windows.ShellExecute(0, nil, windows.StringToUTF16Ptr(url), nil, nil, windows.SW_SHOWNORMAL)
